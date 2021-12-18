e2c237daea0840c94dbb3628e09aeb84fbce2e46
Hammad Asad <hammad.asad444@gmail.com>
2021-12-18 12:37:38 +0500

    local changing

--- app/store/index.ts
+++ app/store/index.ts
@@ -1,6 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {userAutoLogin} from '@app/store/user/login/actions'
 
 import rootReducer from './rootReducer';
 import { rootSaga } from './rootSaga';
@@ -19,4 +21,25 @@ const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 // Run the saga
 sagaMiddleware.run(rootSaga);
 
+// const getAsyncStorage = () => {
+//     return (dispatch)=>{
+//         AsyncStorage.getItem('user')
+//         .then(res=>{
+//             if(res){
+//                 let user = JSON.parse(res);
+//                 dispatch(userAutoLogin(user));
+//             }
+//         })
+//     }
+// }
+// store.dispatch(getAsyncStorage());
+AsyncStorage.getItem('user')
+.then(res=>{
+    if(res){
+        let user = JSON.parse(res) as any;
+        console.log('store disatch', user);
+        
+        store.dispatch(userAutoLogin(user))
+    }
+})
 export default store;
